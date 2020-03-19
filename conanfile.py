dc34dddb860d41668ffef55fc86fd0e04cf03c86
Michael Gardner <shellshocked2003@gmail.com>
2020-03-19 13:37:05 -0700

    Conan file for building Clp using coinbrew.sh

---
+++ conanfile.py
@@ -0,0 +1,62 @@
+from conans import ConanFile, CMake, tools
+import os
+# import subprocess
+
+class smeltConan(ConanFile):
+    name = "Clp"
+    version = "1.17.5"
+    description = "Open-Source Linear Programming Solver"
+    license = "Eclipse Public License - v1.0"
+    author = "Michael Gardner mhgardner@unr.edu"
+    url = "https://github.com/coin-or/Clp"
+    settings = {"os": ["Linux", "Macos"], "build_type": None, "compiler": None, "arch": ["x86_64"]}
+    options = {"shared": [True, False]}
+    default_options = {"shared": False}
+    build_policy = "missing"
+
+    # Custom attributes for Bincrafters recipe conventions
+    _source_subfolder = "source_subfolder"
+    _build_subfolder = "build_subfolder"
+    _install_subfolder = "install_subfolder"
+
+    # scm = {
+    #     "type": "git",  # Use "type": "svn", if local repo is managed using SVN
+    #     "subfolder": _source_subfolder,
+    #     "url": "auto",
+    #     "revision": "auto"
+    # }
+    
+    def source(self):
+       git = tools.Git(folder=self._build_subfolder)
+       git.clone("https://github.com/shellshocked2003/Coin-Clp.git", "master")
+    
+    def build(self):
+        # Build using coinbrew script        
+        if self.options.shared:
+            self.run(self._build_subfolder + "/coinbrew.sh build Clp:releases/1.17.5 --parallel-jobs=8 --prefix=" + self._install_subfolder)
+        else:
+            self.run(self._build_subfolder + "/coinbrew.sh build Clp:releases/1.17.5 --fully-static --parallel-jobs=8 --prefix=" + self._install_subfolder)
+
+    def package(self):
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        self.copy("*.h", dst="include", src=self._install_subfolder + "/include")
+        self.copy("*.hpp", dst="include", src=self._install_subfolder + "/include")        
+        self.copy("*.tcc", dst="include", src=self._install_subfolder + "/include")
+
+        self.copy("*.dll", dst="bin", src=self._install_subfolder + "/bin")
+        self.copy("*.lib", dst="lib", src=self._install_subfolder + "/lib")
+        self.copy("*.so", dst="lib", src=self._install_subfolder + "/lib")
+        self.copy("*.dylib", dst="lib", src=self._install_subfolder + "/lib")
+        self.copy("*.a", dst="lib", src=self._install_subfolder + "/lib")
+        self.copy("*.la", dst="lib", src=self._install_subfolder + "/lib")        
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.includedirs = ['include']
+        self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
+        # Add to path so shared objects can be found        
+        if self.options.shared:
+            self.env_info.PATH.append(os.path.join(self.package_folder, "lib"))
+            self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))                        
+            self.env_info.LD_LIBRARY_PATH.append(os.path.join(self.package_folder, "lib"))
+            self.env_info.DYLD_LIBRARY_PATH.append(os.path.join(self.package_folder, "lib"))
